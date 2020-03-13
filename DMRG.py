35e35bd5b2487f62d7b663b20ecf9a5bbbadce76
aaronleesander <61705296+aaronleesander@users.noreply.github.com>
2020-03-13 23:54:42 +0100

    Update DMRG.py

--- DMRG.py
+++ DMRG.py
@@ -8,12 +8,28 @@
 # Renormalization Group (DMRG) for finding the ground state of an arbitrary
 # Hamiltonian
 
-# Steps:
-# 1. Initialize a Hamiltonian tensor for N particles
-# 2. Decompose it with the MPS method
-# 3. Apply variational method to determine energy value
-# 4. Minimize energy one matrix at a time while holding others constant
-# 5. Apply DMRG? (TBD)
-
 import numpy as np
 
+# Initialization
+pauli_z = np.array([[1,0],
+                    [0,-1]])
+
+pauli_x = np.array([[0,1],
+                   [1,0]])
+
+g = 1
+
+# Matrix Product States
+# First lattice position (1x3x2x2)
+A_1 = np.array([np.identity(2), pauli_z, g*pauli_x])
+A_1 = A_1[np.newaxis, :] # Reshape to add the first index
+
+# Last lattice position (3x1x2x2)
+A_N = np.array([[g*pauli_x],
+               [pauli_z],
+               [np.identity(2)]])
+
+# Middle lattice positions (3x3x2x2)
+A_i = np.array([[np.identity(2), pauli_z, g*pauli_x],
+                [0, 0, pauli_z],
+                [0, 0, np.identity(2)]])
