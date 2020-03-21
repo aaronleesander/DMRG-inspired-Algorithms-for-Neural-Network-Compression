574c9a1805b19883ed05dd686f1cfb50ebad9f22
aaronleesander <61705296+aaronleesander@users.noreply.github.com>
2020-03-21 22:12:46 +0100

    Changed class defintion of Hamiltonian

--- DMRG.py
+++ DMRG.py
@@ -10,39 +10,18 @@
 
 import numpy as np
 
-# Initialization
-pauli_z = np.array([[1,0],
-                    [0,-1]])
-
-pauli_x = np.array([[0,1],
-                   [1,0]])
-
-zero = np.zeros((2,2))
-
-identity = np.identity(2)
-
-# Interaction parameter
-g = 1
-# Lattice positions
-N = 10
-
 class Hamiltonian:
-    # Matrix Product States
-    # First lattice position (1x3x2x2)->(3x2x2)
-    left_bound = np.array([identity, pauli_z, g*pauli_x])
-    
-    # Middle lattice positions (3x3x2x2)
-    middle = np.array([np.array([identity, pauli_z, g*pauli_x]),
-                       np.array([zero, zero, pauli_z]),
-                       np.array([zero, zero, np.identity(2)])])
 
-    # Last lattice position (3x1x2x2)->3x2x2
-    right_bound = np.array([[g*pauli_x],
-                               [pauli_z],
-                               [identity]])
-    right_bound = np.squeeze(right_bound) # Removes unnecessary index 
+    def __init__(self, left_bound, inner, right_bound):
+        # First lattice position (1x3x2x2)->(3x2x2)
+        self.left_bound = left_bound
+        # Middle lattice positions (3x3x2x2)
+        self.inner = inner
+        # Last lattice position (3x1x2x2)->3x2x2
+        self.right_bound = right_bound
 
-### TODO: Define Wavefunction
+
+### TODO: Define Wavefunction class
 
 def contract_from_left(A, B, pos):
     if B.ndim == 4: # Inner lattice positions
@@ -55,12 +34,38 @@ def contract_from_left(A, B, pos):
 
     return tensor
 
-H = Hamiltonian()
+# Initialization
+pauli_z = np.array([[1,0],
+                    [0,-1]])
+
+pauli_x = np.array([[0,1],
+                   [1,0]])
+
+zero = np.zeros((2,2))
+identity = np.identity(2)
+# Interaction parameter
+g = 1
+# Lattice positions
+N = 10
+
+# Initialization of Hamiltonian
+left_bound = np.array([identity, pauli_z, g*pauli_x])
+    
+inner = np.array([np.array([identity, pauli_z, g*pauli_x]),
+                   np.array([zero, zero, pauli_z]),
+                   np.array([zero, zero, np.identity(2)])])
+
+right_bound = np.array([[g*pauli_x],
+                        [pauli_z],
+                        [identity]])
+right_bound = np.squeeze(right_bound) # Removes unnecessary index 
+
+H = Hamiltonian(left_bound, inner, right_bound)
 
 # Initialize with first lattice position
 tensor = H.left_bound
 # Loop over all the inner lattice positions
 for i in range(2, N):
-     tensor = contract_from_left(tensor, H.middle, i)
+     tensor = contract_from_left(tensor, H.inner, i)
 # Final lattice position has different indices so is done alone
 E = contract_from_left(tensor, H.right_bound, N)
