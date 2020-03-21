e8e6a2b2dc89e1dc5e206eb2d1f4f56e7f0a2984
aaronleesander <61705296+aaronleesander@users.noreply.github.com>
2020-03-21 21:56:51 +0100

    Contraction from left works properly

--- DMRG.py
+++ DMRG.py
@@ -21,40 +21,46 @@ zero = np.zeros((2,2))
 
 identity = np.identity(2)
 
+# Interaction parameter
 g = 1
+# Lattice positions
+N = 10
 
 class Hamiltonian:
     # Matrix Product States
-    # First lattice position (1x3x2x2)
+    # First lattice position (1x3x2x2)->(3x2x2)
     left_bound = np.array([identity, pauli_z, g*pauli_x])
-    left_bound = left_bound[np.newaxis, :] # Reshape to add the first index
     
     # Middle lattice positions (3x3x2x2)
     middle = np.array([np.array([identity, pauli_z, g*pauli_x]),
                        np.array([zero, zero, pauli_z]),
                        np.array([zero, zero, np.identity(2)])])
 
-    # Last lattice position (3x1x2x2)
+    # Last lattice position (3x1x2x2)->3x2x2
     right_bound = np.array([[g*pauli_x],
                                [pauli_z],
                                [identity]])
+    right_bound = np.squeeze(right_bound) # Removes unnecessary index 
 
-def contract_from_left(A, B):
-    tensor = np.einsum('ijk,aibc->ajbkc', A, B)
-    tensor = np.reshape(tensor, (3,4,4)) # Collapses indices to (c, a*j, b*k)
-                                         # TODO: Verify correct reshape
+### TODO: Define Wavefunction
+
+def contract_from_left(A, B, pos):
+    if B.ndim == 4: # Inner lattice positions
+        tensor = np.einsum('ijk,aibc->ajbkc', A, B)
+        tensor = np.reshape(tensor, (3,2**pos,2**pos)) # Collapses indices to (c, a*j, b*k) for i particles
+                                                       # TODO: Verify correct reshape
+    if B.ndim == 3: # Final lattice position
+        tensor = np.einsum('ijk,iab->jkab', A, B)
+        tensor = np.reshape(tensor, (2**pos,2**pos))
+
     return tensor
 
 H = Hamiltonian()
 
-first_collapse = np.einsum('ijk, aibc->ajbkc', H.left_bound, H.middle)
-first_collapse = np.reshape(first_collapse, (3,4,4))
-
-N = 3
-inner_tensor = first_collapse
-for i in range(1, N-1):
-    inner_tensor = np.einsum('ijk, aibc->ajbkc', inner_tensor, H.middle)
-    inner_tensor = np.reshape(inner_tensor, (3,2,2))
-
-last_collapse = np.einsum('ijk,iab->jkab', inner_tensor, H.right_bound)
-last_collapse = np.reshape(last_collapse, (2^N,2^N))
+# Initialize with first lattice position
+tensor = H.left_bound
+# Loop over all the inner lattice positions
+for i in range(2, N):
+     tensor = contract_from_left(tensor, H.middle, i)
+# Final lattice position has different indices so is done alone
+E = contract_from_left(tensor, H.right_bound, N)
