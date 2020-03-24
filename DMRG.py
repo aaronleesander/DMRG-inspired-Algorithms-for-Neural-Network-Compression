c0272ba880ec5303fe8e34dd19922d65e19e1b0d
aaronleesander <61705296+aaronleesander@users.noreply.github.com>
2020-03-24 21:41:30 +0100

    export to py and pep8 cleanup

--- DMRG.py
+++ DMRG.py
@@ -2,8 +2,8 @@
 # Author: Aaron Sander
 # Date: March 2020
 
-# This program is used for initial learning of tensor network methods to be
-# used in my bachelor thesis.
+# This program is used for initial learning of tensor network methods
+# to be used in my bachelor thesis.
 # It is an implementation of Matrix Product States (MPS) and Density Matrix
 # Renormalization Group (DMRG) for finding the ground state of an arbitrary
 # Hamiltonian
@@ -14,7 +14,7 @@ import numpy as np
 
 
 ######################## CLASSES ##############################################
-class MPO:
+class matrixProductOperator:
     def __init__(self, left_bound, inner, right_bound):
         # Leftmost lattice position
         self.left_bound = left_bound
@@ -24,7 +24,7 @@ class MPO:
         self.right_bound = right_bound
 
 
-class MPS:
+class matrixProductState:
     def __init__(self, left_bound, inner, right_bound):
         self.left_bound = left_bound
         self.inner = inner
@@ -32,6 +32,9 @@ class MPS:
 
 
 ###################### FUNCTIONS ##############################################
+# NOTE: All directions of contractions have been tested and work correctly.
+#       This was verified by checking if result is equal for going
+#       left<->right and up<->down.
 # Used to contract Hamiltonian horizontally from A->B
 def contract_horizontal(A, B, dir):
     if B.ndim == 4:  # Applied to inner lattice position
@@ -43,10 +46,20 @@ def contract_horizontal(A, B, dir):
         tensor = np.reshape(tensor, (A.shape[0], A.shape[1]*B.shape[2], A.shape[2]*B.shape[3]))
 
     if B.ndim == 3:  # Applied to outer lattice position
-        if dir == 'right' or 'left':  # Seems to be direction-independent
+        if dir == 'right' or 'left':  # Can be removed, left for readability
             tensor = np.einsum('ijk, iab->jakb', A, B)
         # Reshape collapses indices to (j*a, k*b)
         tensor = np.reshape(tensor, (A.shape[1]*B.shape[1], A.shape[2]*B.shape[2]))
+
+    if B.ndim == 2:
+        if dir == 'right':
+            tensor = np.einsum('i, ia->a', A, B)
+        if dir == 'left':
+            tensor = np.einsum('i, ai->a', A, B)
+
+    if B.ndim == 1: # Inner Product
+        if dir == 'right' or 'left':  # Can be removed, left for readability
+            tensor = np.einsum('i, i', A, B)
 
     return tensor
 
@@ -86,6 +99,35 @@ def contract_vertical(A, B, dir):
 
     return tensor
 
+
+def calculateExpectation(MPS_bra, MPO, MPS_ket, vert_dir, horiz_dir):
+    # Initialize list of tensors
+    tensor = [None]*N
+
+    # Contract <MPS|MPO|MPS> at each lattice position
+    # Down: Bra -> MPO -> Ket
+    # Up: Ket -> MPO -> Bra
+    for i in range(0, N):
+        if vert_dir == 'down':
+            first_contraction = contract_vertical(MPS_bra[i], MPO[i], vert_dir)
+            tensor[i] = contract_vertical(first_contraction, MPS_ket[i], vert_dir)
+        if vert_dir == 'up':
+            first_contraction = contract_vertical(MPS_ket[i], MPO[i], vert_dir)
+            tensor[i] = contract_vertical(first_contraction, MPS_bra[i], vert_dir)
+
+    # Contract each tensor created from above
+    # Left and right necessary for scanning in DMRG
+    if horiz_dir == 'right':
+        E = tensor[0]
+        for i in range(1, len(tensor)):
+            E = contract_horizontal(E, tensor[i], horiz_dir)
+    if horiz_dir == 'left':
+        E = tensor[-1]
+        for i in range(len(tensor)-2, -1, -1):
+            E = contract_horizontal(E, tensor[i], horiz_dir)
+
+    return E
+
 # TODO: Verify correct reshape
 # TODO: Determine if vertical/horizontal can be generalized and combined
 # TODO: Use tensordot for better or standardize indices for better readability
@@ -106,7 +148,7 @@ identity = np.identity(2)
 # Interaction parameter
 g = 1
 # Particles (Total lattice positions)
-N = 3
+N = 15
 
 # Initialization of Hamiltonian MPO (entries done by hand)
 # Dimensions (1x3x2x2)->(3x2x2)
@@ -123,7 +165,11 @@ right_bound = np.array([[g*pauli_x],
                         [identity]])
 right_bound = np.squeeze(right_bound)  # Removes unnecessary index
 
-MPO = MPO(left_bound, inner, right_bound)
+MPO_initial = matrixProductOperator(left_bound, inner, right_bound)
+MPO = [MPO_initial.left_bound] + [MPO_initial.inner]*(N-2) + [MPO_initial.right_bound]
+# NOTE: The MPO never gets modified, so for a possible performance boost
+#       we could just use the MPO_initial. For readability by indexing over
+#       each position, we will expand it anyway so that it matches the MPS.
 
 
 ######################### INITIALIZATION MPS ##################################
@@ -132,7 +178,7 @@ MPO = MPO(left_bound, inner, right_bound)
 #             |-> )              |-x-| )
 
 # Bond Dimension
-d = 5
+d = 2
 
 up_ket = np.zeros((d, 1))
 up_ket[0,0] = 1
@@ -162,85 +208,16 @@ A_N = np.squeeze(A_N)
 A_N_dagger = np.array(np.matrix(A_N).H)
 A_N_dagger = np.squeeze(A_N_dagger)
 
-MPS_bra = MPS(A_1_dagger, A_i_dagger, A_N_dagger)
-MPS_ket = MPS(A_1, A_i, A_N)
+# Initialization of MPS elements
+MPS_ket_initial = matrixProductState(A_1, A_i, A_N)
+MPS_bra_initial = matrixProductState(A_1_dagger, A_i_dagger, A_N_dagger)
+# The inner matrices will not stay the same so we a matrix for each lattice position
+MPS_ket = [MPS_ket_initial.left_bound] + [MPS_ket_initial.inner]*(N-2) + [MPS_ket_initial.right_bound]
+MPS_bra = [MPS_bra_initial.left_bound] + [MPS_bra_initial.inner]*(N-2) + [MPS_bra_initial.right_bound]
 
-# TODO: Find a better notation than using MPS_bra, MPS_ket
-# TODO: MPS should not have left_bound, inner, right_bound because
-#       all inner tensors need to be modified, unlike MPO where
-#       all inner tensors remain the same.
-# TODO: Verify if A_i == A_i_dagger is correct
+# TODO: Verify if A_i == A_i_dagger is correct aka if squeeze is necessary/working correctly
 # NOTE: May be necessary to reshape wavefunctions to D x D x 2
 
-
-######################## TESTING ##############################################
-##################### CONTRACT HAMILTONIAN L->R ###############################
-# Initialize with first lattice position
-tensor = MPO.left_bound
-# Loop over all the inner lattice positions
-for i in range(2, N):
-    tensor = contract_horizontal(tensor, MPO.inner, "right")
-# Final lattice position has different indices so is done alone
-E_L = contract_horizontal(tensor, MPO.right_bound, "right")
-
-
-##################### CONTRACT HAMILTONIAN R->L ###############################
-# Initialize with first lattice position
-tensor = MPO.right_bound
-# Loop over all the inner lattice positions
-for i in range(2, N):
-    tensor = contract_horizontal(tensor, MPO.inner, "left")
-# Final lattice position has different indices so is done alone
-E_R = contract_horizontal(tensor, MPO.left_bound, "left")
-
-if E_L.all() == E_R.all():
-    print("Hamiltonian contracts properly in both directions")
-
-
-####################### CONTRACT FIRST LATTICE POSITION DOWNWARDS #############
-# Dimensions (3*d x 2)
-first = contract_vertical(MPS_bra.left_bound, MPO.left_bound, 'down')
-# Dimensions (3*d*d)
-pos1_contract_down = contract_vertical(first, MPS_ket.left_bound, 'down')
-
-
-####################### CONTRACT FIRST LATTICE POSITION UPWARDS ###############
-# Dimensions (3*d x 2)
-first = contract_vertical(MPS_ket.left_bound, MPO.left_bound, 'up')
-# Dimensions (3*d*d)
-pos1_contract_up = contract_vertical(first, MPS_bra.left_bound, 'up')
-
-if pos1_contract_down.all() == pos1_contract_up.all():
-    print("Left lattice position contracts properly in both directions")
-
-
-################## CONTRACT INNER LATTICE POSITION DOWNARDS ###################
-# Dimensions (3d x 3d x 2)
-first = contract_vertical(MPS_bra.inner, MPO.inner, 'down')
-# Dimensions
-posI_contract_down = contract_vertical(first, MPS_ket.inner, 'down')
-
-################## CONTRACT INNER LATTICE POSITION UPWARDS ####################
-# Dimensions (3d x 3d x 2)
-first = contract_vertical(MPS_ket.inner, MPO.inner, 'up')
-# Dimensions (3*d*d)
-posI_contract_up = contract_vertical(first, MPS_bra.inner, 'up')
-
-if posI_contract_down.all() == posI_contract_up.all():
-    print("Inner lattice position contracts properly in both directions")
-
-
-################## CONTRACT LAST LATTICE POSITION DOWNWARDS #################
-# Dimensions (3d x 2)
-first = contract_vertical(MPS_bra.right_bound, MPO.right_bound, 'down')
-# Dimensions (3*d*d)
-posN_contract_down = contract_vertical(first, MPS_ket.right_bound, 'down')
-
-################## CONTRACT LAST LATTICE POSITION UPWARDS #################
-# Dimensions (3d x 2)
-first = contract_vertical(MPS_ket.right_bound, MPO.right_bound, 'up')
-# Dimensions (3*d*d)
-posN_contract_up = contract_vertical(first, MPS_bra.right_bound, 'up')
-
-if posN_contract_down.all() == posN_contract_up.all():
-    print("Right lattice position contracts properly in both directions")
+######################### TESTING #############################################
+E_R = calculateExpectation(MPS_bra, MPO, MPS_ket, 'down', 'right')
+E_L = calculateExpectation(MPS_bra, MPO, MPS_ket, 'down', 'left')
